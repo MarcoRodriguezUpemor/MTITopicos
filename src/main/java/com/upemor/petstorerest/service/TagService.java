4720a7a9c5b84619d107b82d0bb597b02225e9e9
Marco Rodriguez <rfmarco1020@gmail.com>
2020-10-31 12:57:26 -0600

    proyecto al dia 31

---
+++ src/main/java/com/upemor/petstorerest/service/TagService.java
@@ -0,0 +1,20 @@
+package com.upemor.petstorerest.service;
+
+import java.util.List;
+
+import com.upemor.petstorerest.model.Tag;
+
+public interface TagService {
+
+	// Operaciones Crud en base de datos
+	
+	List<Tag> listAllTags();
+	
+	Tag findById(int id);
+	
+	void createTag(Tag tag);
+	
+	Tag updateTag(int id,Tag tag);
+	
+	void deleteTag(int id);
+}
